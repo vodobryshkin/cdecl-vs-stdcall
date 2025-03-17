dc8259973316343e5f94c6f7b73807167ff84eed
vodobryshkin <dobryshkinvladimir@yandex.ru>
2025-03-18 00:35:29 +0300

    implemented support for negative numbers

--- 1_multiply/calg.c
+++ 1_multiply/calg.c
@@ -1,10 +1,50 @@
 #include <stdio.h>
 
+int abs(int a) {
+    int res = a;
+
+    if (a < 0) {
+        res = -res;
+    }
+
+    return res;
+}
+
+int get_sign(int a) {
+    int sign = 1;
+
+    if (a < 0) {
+        sign = -1;
+    }
+
+    return sign;
+}
+
+int get_divison_sign(int a, int b) {
+    int zn = 1;
+
+    int a_sign = get_sign(a);
+    int b_sign = get_sign(b);
+
+    if (a_sign != b_sign) {
+        zn = -1;
+    }
+
+    return zn;
+}
+
 int multiply(int a, int b) {
     int c = 0;
+    int sign = get_divison_sign(a, b);
+    int abs_a = abs(a);
+    int abs_b = abs(b);
 
-    for (int i = 0; i < b; i++) {
-        c = c + a;
+    for (int i = 0; i < abs_b; i++) {
+        c = c + abs_a;
+    }
+
+    if (sign == -1) {
+        c = -c;
     }
 
     return c;
@@ -12,6 +52,6 @@ int multiply(int a, int b) {
 
 
 int main() {
-    printf("%d\n", multiply(4, 1));
+    printf("%d\n", multiply(-4, -10));
     return 0;
 }
--- 1_multiply/cdecl.asm
+++ 1_multiply/cdecl.asm
@@ -1,7 +1,7 @@
 org 0x100
 
-a:  word 0x0005
-b:  word 0x0002
+a:  word 0x000A
+b:  word 0xFFFE
 
 start:
     ld b
@@ -12,49 +12,196 @@ start:
     
     call $multiply
 
+    swap
+    pop
+
+    swap
+    pop
+
     hlt
 
-multiply:
-    ; prologue
-    push ; memory for c
-    push ; memory for i
+abs:
+    push
 
-    ; body
-    cla ; c
-    st  &1
+    ld &2
+    st &0
 
-    cla ; i
-    st  &0
-
-    ; loop for
-    for_body:
-        ld  &0 ; next i
-        cmp &4 ; comparison with b
-        bge for_done ; go to for_exit if 
-
-        ld  &1
-        add &3 ; c = c + a
-        st &1
-
+    bpl end_of_func_abs
+    neg
+    st &0
+    end_of_func_abs:
         ld &0
-        inc
-        st  &0 ; i++
 
-        jump for_body  
-    for_done:
-        ld  &1
-    ; end
-    swap
-    pop
+        swap
+        pop
 
-    swap
-    pop
+        swap
+        st &1
+        pop
 
-    swap
+        ret
+
+get_sign:
+    push
+
+    ld #0x01
+    st &0
+
+    ld &2
+    cmp #0x00
+
+    bge end_of_func_get_sign
+    ld #0xFF
+    st &0
+
+    end_of_func_get_sign:
+        ld &0
+
+        swap
+        pop
+
+        swap
+        st &1
+        pop
+
+        ret
+
+get_divison_sign:
+    push
+    push
+    push
+
+    ; zn
+    ld #0x01
     st &2
-    pop
 
-    swap
-    pop
+    ; a_sign
+    ld &4
+    push
+    call $get_sign
+    st &1
 
-    ret
+    ; b_sign
+    ld &5
+    push
+    call $get_sign
+    st &0
+
+    ; if a_sign != b_sign => zn = FFFF
+    ld &0
+    cmp &1
+
+    beq end_of_func_get_divison_sign
+    ld #0xFF
+    st &2
+
+    end_of_func_get_divison_sign:
+        ld &2
+
+        swap
+        pop
+
+        swap
+        pop
+
+        swap
+        pop
+
+        swap
+        st &2
+        pop
+
+        swap
+        pop
+
+        ret
+
+multiply:
+    push
+    push
+    push
+    push
+    push
+
+    ; c
+    cla
+    st &4
+
+    ; sign
+    push
+    push
+
+    ld &9
+    st &1
+    
+    ld &8
+    st &0
+
+    call $get_divison_sign
+    st &3
+
+    ; abs_a
+    push
+
+    ld &7
+    st &0
+
+    call $abs
+    st &2
+
+    ; abs_b
+    push
+
+    ld &8
+    st &0
+
+    call $abs
+    st &1
+
+    ; i
+    cla
+    st &0
+
+    multiply_start_loop:
+        ld &0
+
+        cmp &1
+        bge multiply_end_loop
+        
+        inc
+        st &0
+        
+        ld &4
+        add &2
+        st &4
+
+        jump multiply_start_loop
+    
+    multiply_end_loop:
+        ld &3
+        cmp #0xFF
+
+        bne end_of_func_multiply
+        ld &4
+        neg
+        st &4
+    
+    end_of_func_multiply:
+        ld &4
+        
+        swap
+        pop
+
+        swap
+        pop
+
+        swap
+        pop
+
+        swap
+        pop
+
+        swap
+        pop
+
+        ret
